d896551253d1c11618a85bec678f1a7f346e8220
Phil Taprogge <philt@pusher.com>
2016-07-28 12:31:02 +0100

    Add distinction between rebase and checkout

--- autocomplete_gitbranch.sh
+++ autocomplete_gitbranch.sh
@@ -1,11 +1,25 @@
+git_takes_branch ()
+{
+	if [[ "checkout" == ${COMP_WORDS[1]} ]]; then
+		if [[ $comp_git_branches =~ ${COMP_WORDS[COMP_CWORD-1]} ]]; then
+			return 1
+		else
+			return 0
+		fi
+	elsif [[ "rebase" == ${COMP_WORDS[1]} ]];
+		return 0
+	else
+		return 1
+	fi
+}
+
 _complete_git_branches ()
 {
         COMPREPLY=()
-        cur="${COMP_WORDS[COMP_CWORD]}"
-        prv="${COMP_WORDS[COMP_CWORD-1]}"
-        if [[ ${prv} == "checkout" ]] ; then
-                comp_git_branches=$(git branch 2>/dev/null | grep -v "^\*")
-                COMPREPLY=( $(compgen -W "${comp_git_branches}" -- "$cur"))
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	comp_git_branches=$(git branch 2>/dev/null | sed s/^[*]*[[:space:]]*//)
+        if git_takes_branch; then
+                COMPREPLY=( $(compgen -W "${comp_git_branches}" -- $cur))
         else
                 COMPREPLY=( $(compgen -f "${COMP_WORDS[${COMP_CWORD}]}" ))
         fi
