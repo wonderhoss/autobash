8b0cabd96fdc08261af18e54816a49e76fb3d731
Phil Taprogge <philt@pusher.com>
2016-08-02 10:12:15 +0100

    Fix warnings

--- autocomplete_gitbranch.sh
+++ autocomplete_gitbranch.sh
@@ -4,8 +4,8 @@ _complete_git_branches ()
         cur="${COMP_WORDS[COMP_CWORD]}"
         prv="${COMP_WORDS[COMP_CWORD-1]}"
         if [[ ${prv} == "checkout" ]] ; then
-                comp_git_branches=`git branch 2>/dev/null | grep -v ^*`
-                COMPREPLY=( $(compgen -W "${comp_git_branches}" -- $cur))
+                comp_git_branches=$(git branch 2>/dev/null | grep -v "^\*")
+                COMPREPLY=( $(compgen -W "${comp_git_branches}" -- "$cur"))
         else
                 COMPREPLY=( $(compgen -f ${COMP_WORDS[${COMP_CWORD}]} ))
         fi
