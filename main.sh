6f5868e70c1383f2c681b58c45910e13ad0e9a28
Phil Taprogge <philt@pusher.com>
2016-08-02 17:06:03 +0100

    Add warning if bash version is lower than 4. Fixes #4

--- main.sh
+++ main.sh
@@ -4,3 +4,11 @@
 source "${AUTOBASH}/git/autocomplete_gitbranch.sh"
 
 complete -o filenames -o bashdefault -F _complete_git_branches git
+
+if [[ ! $BASH_VERSION == 4* ]]; then
+  echo "Notice: Some autobash functionality requires bash version 4."
+  echo "Your bash is $BASH_VERSION and those functions will be disabled."
+  echo "For more information see" 
+  echo "https://gist.github.com/gargath/49ece1408aa66e99c1b26f35f812e586#file-autobash4-md"
+fi
+
