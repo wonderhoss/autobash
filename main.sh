ce2b901ce7f13358f40ccdae0e6f2787d1bc2011
Phil Taprogge <signup@lightweaver.info>
2016-08-03 21:23:52 +0100

    Development (#21)
    * Refactor git subcommand completeion into functions
    
    * Resolve open shellcheck warnings. Fixes #13
    
    * Move git script into subdir
    
    * Add warning if bash version is lower than 4. Fixes #4
    
    * Add checks before sourcing
    
    * Fix missing array index

--- main.sh
+++ main.sh
@@ -1,6 +1,24 @@
 # Entrypoint script. Source from .bash_profile or similar
 
-# shellcheck source=./autocomplete_gitbranch.sh
-source "${AUTOBASH}/autocomplete_gitbranch.sh"
+if [[ "$0" == "${BASH_SOURCE[0]}" ]]; then
+  echo "Error: This script needs to be sourced instead of executed."
+  exit 1
+fi
+
+if [ -z "${AUTOBASH}" ]; then
+  echo "Error: Please set \$AUTOBASH to point to the autobash directory before sourcing this file."
+  return 1
+fi
+
+# shellcheck source=./git/autocomplete_gitbranch.sh
+source "${AUTOBASH}/git/autocomplete_gitbranch.sh"
 
 complete -o filenames -o bashdefault -F _complete_git_branches git
+
+if [[ ! $BASH_VERSION == 4* ]]; then
+  echo "Notice: Some autobash functionality requires bash version 4."
+  echo "Your bash is $BASH_VERSION and those functions will be disabled."
+  echo "For more information see" 
+  echo "https://gist.github.com/gargath/49ece1408aa66e99c1b26f35f812e586#file-autobash4-md"
+fi
+
