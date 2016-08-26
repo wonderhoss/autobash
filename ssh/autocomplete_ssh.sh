66c6f29c77b96e32046a8b083313a6545370fc27
Phil Taprogge <philt@pusher.com>
2016-08-26 11:08:57 +0100

    Improve ssh host scanning using awk magic

--- ssh/autocomplete_ssh.sh
+++ ssh/autocomplete_ssh.sh
@@ -1,17 +1,9 @@
 _complete_ssh_hosts ()
 {
-        COMPREPLY=()
-        cur="${COMP_WORDS[COMP_CWORD]}"
-        comp_ssh_hosts=$(cat ~/.ssh/known_hosts | \
-			sed s/[,\ ].*\$//| \
-                        grep -v ^# | \
-			grep -v "\[" | \
-			sort -u ;
-                cat ~/.ssh/config | \
-                        grep "^Host " | \
-			grep -v \* |
-                        cut -f 2 -d ' '
-                )
-        COMPREPLY=( $(compgen -W "${comp_ssh_hosts}" -- $cur))
-        return 0
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  comp_ssh_hosts=$(awk 'BEGIN{FS=",| "} !/^[[#]|^$/ { print $1 }' ~/.ssh/known_hosts;
+                   awk '/^Host.*/ { if ($2 ~ !/^\*.*/) print $2}' ~/.ssh/config)
+  COMPREPLY=( $(compgen -W "${comp_ssh_hosts}" -- "$cur"))
+  return 0
 }
