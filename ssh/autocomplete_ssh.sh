4f2817fd2f35dabb53ddd7bf7fc49f6712dda594
Phil Taprogge <philt@pusher.com>
2017-03-01 17:14:48 +0000

    Fix auto-complete for user@ ssh partials. Fixes #20.

--- ssh/autocomplete_ssh.sh
+++ ssh/autocomplete_ssh.sh
@@ -4,7 +4,9 @@ _complete_ssh_hosts ()
   cur="${COMP_WORDS[COMP_CWORD]}"
   comp_ssh_hosts=$(awk 'BEGIN{FS=",| "} !/^[[#]|^$/ { print $1 }' ~/.ssh/known_hosts;
                    awk '/^Host.*/ { if ($2 ~ !/^\*.*/) print $2}' ~/.ssh/config)
-  if [[ "-i" == "${COMP_WORDS[COMP_CWORD-1]}" ]]; then
+  if [[ "${COMP_WORDS[COMP_CWORD-1]}" == "@" ]]; then
+    COMPREPLY=( @$(compgen -W "${comp_ssh_hosts}" -- "$cur"))
+  elif [[ "-i" == "${COMP_WORDS[COMP_CWORD-1]}" ]]; then
     COMPREPLY=( $(compgen -f "${COMP_WORDS[${COMP_CWORD}]}" ))
   else
     COMPREPLY=( $(compgen -W "${comp_ssh_hosts}" -- "$cur"))
