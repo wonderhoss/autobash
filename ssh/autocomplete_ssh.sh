ab29b0afca360cd82d450a4d74dcefb8e1576c76
Phil Taprogge <philt@pusher.com>
2017-03-01 16:36:27 +0000

    Ensure ssh -i autocompletes files rather than hosts. Fixes #17

--- ssh/autocomplete_ssh.sh
+++ ssh/autocomplete_ssh.sh
@@ -4,6 +4,10 @@ _complete_ssh_hosts ()
   cur="${COMP_WORDS[COMP_CWORD]}"
   comp_ssh_hosts=$(awk 'BEGIN{FS=",| "} !/^[[#]|^$/ { print $1 }' ~/.ssh/known_hosts;
                    awk '/^Host.*/ { if ($2 ~ !/^\*.*/) print $2}' ~/.ssh/config)
-  COMPREPLY=( $(compgen -W "${comp_ssh_hosts}" -- "$cur"))
+  if [[ "-i" == "${COMP_WORDS[COMP_CWORD-1]}" ]]; then
+    COMPREPLY=( $(compgen -f "${COMP_WORDS[${COMP_CWORD}]}" ))
+  else
+    COMPREPLY=( $(compgen -W "${comp_ssh_hosts}" -- "$cur"))
+  fi
   return 0
 }
